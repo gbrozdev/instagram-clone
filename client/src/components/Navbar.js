ac4ec3155d2cc788d2f2aff87b513d82efa682f2
gbrozdev <gbrozdev@gmail.com>
2022-08-14 09:27:47 +0530

    updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -60,7 +60,8 @@ const NavBar = ()=>{
     return(
         <nav>
         <div className="nav-wrapper white">
-          <Link to={state?"/":"/signin"} className="brand-logo left">Instagram</Link>
+       
+          <Link to={state?"/":"/signin"} className="brand-logo left">gbrozgram</Link>
           <ul id="nav-mobile" className="right">
              {renderList()}
   
--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -39,7 +39,7 @@ const SignIn  = ()=>{
    return (
       <div className="mycard">
           <div className="card auth-card input-field">
-            <h2>Instagram</h2>
+            <h2>gbrozgram</h2>
             <input
             type="text"
             placeholder="email"
