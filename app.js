4c87ce1e8f7b774fccc3e187f4ce5fe18de0f0a4
gbrozdev <gbrozdev@gmail.com>
2022-08-14 06:47:24 +0530

    mongo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('./config/keys')
+const {MONGOURI} = require('./config/prod')
 
 
 mongoose.connect(MONGOURI,{
@@ -10,6 +10,7 @@ mongoose.connect(MONGOURI,{
     useUnifiedTopology: true
 
 })
+
 mongoose.connection.on('connected',()=>{
     console.log("conneted to mongo yeahh")
 })
